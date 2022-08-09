5333c34cc21280ae944658382de9297790850635
Jordan McGhee <jordanmcghee95@gmail.com>
2022-08-09 17:35:25 -0400

    Created hook to send http requests easier

---
+++ frontend/getfit/src/Hooks/useFetch.js
@@ -0,0 +1,41 @@
+import { useState } from "react"
+
+export const useFetch = (props) => {
+    const [ isLoading, setIsLoading ] = useState(false)
+    const [ hasError, setHasError ] = useState(false)
+
+    const sendRequest = useCallback(
+        async (url, method = "GET", headers = {}, body = null) => {
+            
+            setIsLoading(true)
+
+            try {
+                const response = await fetch(url, {
+                    method,
+                    body,
+                    headers
+                })
+
+                const responseData = await response.json()
+
+                if (!response.ok) {
+                    throw new Error(responseData.message)
+                }
+
+                setIsLoading(false)
+                
+                return responseData
+            } catch(err) {
+                console.log(err)
+                setHasError(err.message || "Something went wrong. Please try again!")
+                setIsLoading(false)
+                throw err
+            }
+    }, [])
+
+    const clearError = () => {
+        setHasError(null)
+    }
+
+    return { isLoading, hasError, sendRequest, clearError }
+}
