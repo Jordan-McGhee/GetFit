68fb060de319e03a02c8cf5f926782ca9565bb9c
Jordan McGhee <jordanmcghee95@gmail.com>
2022-10-06 21:19:42 -0400

    Added lazy loading, production build, and styling to NotFound page

--- frontend/getfit/src/App.js
+++ frontend/getfit/src/App.js
@@ -1,16 +1,5 @@
-import React from "react"
+import React, { Suspense } from "react"
 import { Route, Routes, Navigate } from "react-router-dom"
-
-// COMPONENT IMPORTS
-import './App.css';
-import MainNav from "./Nav/MainNav";
-import HomePage from "./Home/HomePage";
-import NewWorkout from "./Pages/Workout/Pages/NewWorkout";
-import ViewWorkout from "./Pages/Workout/Pages/ViewWorkout";
-import EditWorkout from "./Pages/Workout/Pages/EditWorkout"
-import AuthPage from "./Pages/AuthPage/AuthPage";
-import WorkoutsPage from "./Pages/Workout/Pages/WorkoutsPage";
-import NotFound from "./Pages/404 Not Found/NotFound";
 
 // CONTEXT
 import { AuthContext } from "./Context/auth-context";
@@ -18,10 +7,29 @@ import { AuthContext } from "./Context/auth-context";
 // AUTH HOOK
 import { useAuth } from "./Hooks/useAuth"
 
+// COMPONENT IMPORTS
+import './App.css';
+import MainNav from "./Nav/MainNav";
+// import HomePage from "./Home/HomePage";
+// import NewWorkout from "./Pages/Workout/Pages/NewWorkout";
+// import ViewWorkout from "./Pages/Workout/Pages/ViewWorkout";
+// import EditWorkout from "./Pages/Workout/Pages/EditWorkout"
+import AuthPage from "./Pages/AuthPage/AuthPage";
+import LoadingSpinner from "./Components/UIElements/LoadingSpinner";
+// import WorkoutsPage from "./Pages/Workout/Pages/WorkoutsPage";
+// import NotFound from "./Pages/404 Not Found/NotFound";
+
+// LAZY IMPORTS
+const HomePage = React.lazy(() => import('./Home/HomePage'))
+const NewWorkout = React.lazy(() => import('./Pages/Workout/Pages/NewWorkout'))
+const ViewWorkout = React.lazy(() => import('./Pages/Workout/Pages/ViewWorkout'))
+const EditWorkout = React.lazy(() => import('./Pages/Workout/Pages/EditWorkout'))
+const WorkoutsPage = React.lazy(() => import('./Pages/Workout/Pages/WorkoutsPage'))
+const NotFound = React.lazy(() => import('./Pages/404 Not Found/NotFound'))
+
 function App() {
 
   const { token, userID, login, logout } = useAuth()
-
 
   let routes
 
@@ -29,7 +37,6 @@ function App() {
     routes = (
       <Routes>
         <Route path="/" element={ <HomePage /> } />
-        <Route path="/auth" element = { <Navigate to ="/" replace = { true } />} />
         <Route path="/workout/all" element = { <WorkoutsPage /> } />
         <Route path="/workout/create" element = { <NewWorkout /> } />
         <Route path="/workout/:workoutID/view" element = { <ViewWorkout /> } />
@@ -63,7 +70,13 @@ function App() {
         <MainNav />
           <div className="min-h-screen pb-6 px-6">
             <div className="max-w-3xl m-auto">
-              { routes }
+              <Suspense fallback = {
+                <div className="center">
+                  <LoadingSpinner />
+                </div>
+              }>
+                { routes }
+              </Suspense>
             </div>
             
           </div>
--- frontend/getfit/src/Pages/404 Not Found/NotFound.js
+++ frontend/getfit/src/Pages/404 Not Found/NotFound.js
@@ -3,13 +3,16 @@ import Button from "../../Components/FormElements/Button";
 
 const NotFound = () => {
     return (
-        <div>
-            <p>The page you were looking for doesn't exist.</p>
-            <Button 
-                link = {'/'}
-                text = "Go Home"
-                type = "button"
-            />
+        <div className="min-h-screen pb-6 px-6 my-4">
+            <div>
+                <p className="text-xl mx-auto my-4 border border-black">The page you were looking for doesn't exist.</p>
+                <Button 
+                    link = {'/'}
+                    text = "Go Home"
+                    type = "button"
+                    className = "button border border-gray-1 rounded-md shadow justify-self-center hover:cursor-pointer hover:scale-105"
+                />
+            </div>
         </div>
     )
 }
